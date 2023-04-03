148480a6456944328d889ddf00a6be4a9b42dd4b
Titus Muthomi <muthomititus+github@gmail.com>
2023-04-03 18:25:56 +0300

    Add intoduction logic

--- lib/models.py
+++ lib/models.py
@@ -1,11 +1,11 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import (create_engine, PrimaryKeyConstraint,
+                        Column, String, Integer, ForeignKey)
 import os
 import sys
 
 sys.path.append(os.getcwd)
-
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer, ForeignKey)
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
@@ -16,13 +16,14 @@ engine = create_engine('sqlite:///db/concerts.db', echo=True)
 #     pass
 
 # Band has many Concerts, a Venue has many Concerts, and a Concert belongs to a Band and to a Venue.
+
+
 class Band(Base):
     __tablename__ = 'bands'
 
     id = Column(Integer, primary_key=True)
     name = Column(String())
     hometown = Column(String())
-
 
     def __repr__(self):
         return f'Band: {self.name}'
@@ -34,23 +35,53 @@ class Band(Base):
         return self.venues
 
 
+# Concert hometown_show()
+# returns true if the concert is in the band's hometown, false if it is not
+
+
+    def hometown_show(self):
+        for concert in self.concerts:
+            if concert.venue.city == self.hometown:
+                return True
+        return False
+
+    def upcoming_shows(self):
+        upcoming_shows = []
+        for concert in self.concerts:
+            if concert.date > datetime.now():
+                upcoming_shows.append(concert)
+        return upcoming_shows
+
+#    Concert introduction()
+# returns a string with the band's introduction for this concert
+# an introduction is in the form:
+# "Hello {insert venue city}!!!!! We are {insert band name} and we're from
+# {insert band hometown}"
+
+    def introduction(self):
+        return f'Hello {self.concerts[0].venue.city}!!!!! We are {self.name} and we are from {self.hometown}'
+
+
 class Venue(Base):
     __tablename__ = 'venues'
 
     id = Column(Integer, primary_key=True)
     title = Column(String())
     city = Column(String())
+    band_id = Column(Integer, ForeignKey('bands.id'))
+
+    band = relationship('Band', backref=backref(
+        'venues', uselist=True, cascade='delete,all'))
 
     def __repr__(self):
         return f'Venue: {self.name}'
-    
+
     def concerts(self):
         return self.concerts
 
     def bands(self):
         return self.bands
 
-    
 
 class Concert(Base):
     __tablename__ = 'concerts'
@@ -60,8 +91,10 @@ class Concert(Base):
     band_id = Column(Integer, ForeignKey('bands.id'))
     venue_id = Column(Integer, ForeignKey('venues.id'))
 
-    band = relationship('Band', backref=backref('concerts', uselist=True, cascade='delete,all'))
-    venue = relationship('Venue', backref=backref('concerts', uselist=True, cascade='delete,all'))
+    band = relationship('Band', backref=backref(
+        'concerts', uselist=True, cascade='delete,all'))
+    venue = relationship('Venue', backref=backref(
+        'concerts', uselist=True, cascade='delete,all'))
 
     def band_name(self):
         return self.band.name
